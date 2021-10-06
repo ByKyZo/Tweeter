6a8ba74d1c271190b08c1fc919581fe566ace1d0
unknown <alex07cmj@gmail.com>
2021-10-06 17:22:14 +0200

    backup

--- server/dist/controllers/authController.js
+++ server/dist/controllers/authController.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var database_1 = require("../database/database");
+var validator_1 = __importDefault(require("validator"));
+// poser question s'il faut envoyer le details des erreurs du back vers le front , ou juste bloquer si erreur
 var AuthController = /** @class */ (function () {
     function AuthController() {
     }
@@ -8,7 +13,23 @@ var AuthController = /** @class */ (function () {
         database_1.db.query('INSERT INTO user VALUES (DEFAULT,"toto","toto","ezreezz")');
     };
     AuthController.register = function (req, res) {
-        res.send({ success: 'yeah request success' });
+        var _a = req.body, firstName = _a.firstName, lastName = _a.lastName, email = _a.email, password = _a.password;
+        if (!validator_1.default.isLength(firstName, { min: 2, max: 30 }) ||
+            !validator_1.default.isLength(lastName, { min: 2, max: 30 }) ||
+            !validator_1.default.isEmail(email) ||
+            !validator_1.default.isLength(password, { min: 4, max: 50 })) {
+            res.status(400).send({ error: 'sign up error' });
+            return;
+        }
+        database_1.db.query('INSERT INTO user VALUES (DEFAULT, ? , ? , ? , ? )', [firstName, lastName, email, password], function (error) {
+            if (error) {
+                console.log('query error : ', error);
+            }
+            else {
+                console.log('query success !');
+            }
+        });
+        res.status(201).send({ firstName: firstName, lastName: lastName, email: email, password: password });
     };
     return AuthController;
 }());
--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 import { db } from '../database/database';
+import validator from 'validator';
+
+// poser question s'il faut envoyer le details des erreurs du back vers le front , ou juste bloquer si erreur
 
 export default class AuthController {
     public static login(req: Request, res: Response) {
@@ -7,6 +10,30 @@ export default class AuthController {
     }
 
     public static register(req: Request, res: Response) {
-        res.send({ success: 'yeah request success' });
+        const { firstName, lastName, email, password } = req.body;
+
+        if (
+            !validator.isLength(firstName, { min: 2, max: 30 }) ||
+            !validator.isLength(lastName, { min: 2, max: 30 }) ||
+            !validator.isEmail(email) ||
+            !validator.isLength(password, { min: 4, max: 50 })
+        ) {
+            res.status(400).send({ error: 'sign up error' });
+            return;
+        }
+
+        db.query(
+            'INSERT INTO user VALUES (DEFAULT, ? , ? , ? , ? )',
+            [firstName, lastName, email, password],
+            (error) => {
+                if (error) {
+                    console.log('query error : ', error);
+                } else {
+                    console.log('query success !');
+                }
+            }
+        );
+
+        res.status(201).send({ firstName, lastName, email, password });
     }
 }
